bb717e63250970aebce3ab8ba147bfc8bfd9de96
Konogan <konogan@gmail.com>
2018-06-07 11:52:29 +0200

    Add : header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,25 @@
 <template>
-  <header>
-    <h1>{{title}}</h1>
-  </header>
+
+  <section class="hero is-primary">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        {{title}}
+      </h1>
+      <h2 class="subtitle">
+        {{stitle}} a moi
+      </h2>
+    </div>
+  </div>
+</section>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: "Mon titre de header"
+      title: "Mon titre de header",
+      stitle: "Mon sous titre de header"
     };
   }
 };
