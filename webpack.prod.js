2f591ea59dffbbecb9ee072bf7f293b781d8e80b
Konogan <konogan@gmail.com>
2018-06-07 12:02:28 +0200

    Add : sass in prod, extract css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const merge = require('webpack-merge');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.common.js');
 const webpack = require('webpack')
@@ -7,9 +7,31 @@ const webpack = require('webpack')
 module.exports = merge(common, {
   mode: 'production',
   devtool: '#source-map',
-
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: "css-loader",
+          options: {
+            sourceMap: true
+          }
+        },
+        {
+          loader: "sass-loader",
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+    }]
+  },
   plugins: [
-    // new ExtractTextPlugin('style.css'),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
