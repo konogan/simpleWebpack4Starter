51815dcaa47b8144560519579e176b85b8fd5a56
Konogan <konogan@gmail.com>
2018-06-07 11:55:11 +0200

    Add : Uglifly

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,22 @@
 const merge = require('webpack-merge');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.common.js');
+const webpack = require('webpack')
 
 module.exports = merge(common, {
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ]
-  },
+  devtool: '#source-map',
+
   plugins: [
-    new ExtractTextPlugin('style.css')
+    // new ExtractTextPlugin('style.css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new UglifyJsPlugin({
+      sourceMap: true,
+    })
   ]
 })
